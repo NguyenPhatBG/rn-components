711b102665a5aefe7444a1b47aba6ff03ff0eef3
joon_young_92 <giatocgiausang1992@gmail.com>
2019-09-17 18:55:28 +0700

    add Spoiler components

---
+++ app/screens/TestSpoiler.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Spoiler from './../components/Spoiler';
+
+export default class TestSpoiler extends Component {
+    state = {
+        params: {
+            accountNumber: '000123456789', // required field
+            countryCode: 'us', // required field
+            currency: 'usd', // required field
+            routingNumber: '110000000', // 9 digits
+            accountHolderName: 'Test holder name',
+            accountHolderType: 'company',
+        },
+        errorParams: {
+            accountNumber: '000123456789', // required field
+            countryCode: 'us', // required field
+            currency: 'abc', // required field
+            routingNumber: '110000000', // 9 digits
+            accountHolderName: 'Test holder name',
+            accountHolderType: 'company',
+        }
+    };
+    
+    renderMandatoryFields = params => (
+        <View style={styles.params}>
+            <Text style={styles.param}>
+                Routing Number: {params.routingNumber}
+            </Text>
+            <Text style={styles.param}>
+                Account Number: {params.accountNumber}
+            </Text>
+            <Text style={styles.param}>
+                Country Code: {params.countryCode}
+            </Text>
+            <Text style={styles.param}>
+                Currency: {params.currency}
+            </Text>
+        </View>
+    );
+
+    render() {
+        const { params, errorParams } = this.state;
+        return (
+            <View style={styles.container}>
+                <Spoiler title="Mandatory Fields">
+                    {this.renderMandatoryFields(params)}
+                </Spoiler>
+                <Spoiler title="Mandatory Fields - Error case" defaultOpen={false}>
+                    {this.renderMandatoryFields(errorParams)}
+                </Spoiler>
+            </View>
+        );  
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    params: {
+        backgroundColor: '#ffffff',
+        borderRadius: 10,
+        padding: 10,
+        alignItems: 'flex-start',
+        margin: 5,
+    }
+});
